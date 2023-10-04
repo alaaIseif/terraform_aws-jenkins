036a84d6f727664158fed4957d16811d2f1576db
alaaIseif <alaaseif.3112@gmail.com>
2023-10-05 01:25:27 +0300

    groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -3,52 +3,86 @@ pipeline {
     
     parameters {
         choice(
-            choices: 'dev\nstaging\nproduction',
+            choices: 'dev\nprod',
             description: 'Select environment',
             name: 'ENVIRONMENT'
         )
     }
-    environment {
+     environment {
         AWS_ACCESS_KEY_ID=credentials('aws_access_key_id')
         AWS_SECRET_ACCESS_KEY=credentials('aws_secret_access_key')
     }
+    
     stages {
-        stage('Prepare Environment') {
+
+        stage("Clone Git Repository") {
+            steps {
+                git(
+                    url: "https://github.com/alaaIseif/terraform_aws-jenkins.git",
+                    branch: "master",
+                    poll: true
+                )
+            }
+        }
+
+        //Initializing
+        stage('Terraform Init') {
+            steps {
+                sh 'terraform init'
+            }
+        }
+        
+        stage('Selecting workspace') {
             steps {
                 script {
-                    def environment = params.ENVIRONMENT
-                    
-                    // Configure AWS credentials
-                    withAWS(credentials: 'aws-credentials-id') {
-                        // Clone the Terraform code from the GitHub repository
-                        git branch: 'main', credentialsId: 'github-credentials-id', url: 'https://github.com/alaaIseif/terraformAWS-handson/tree/4e50c078c224c46bcdd01f998065b84641950a7c/Day2'
-                        
-                        // Change to the cloned repository directory
-                        dir('repo') {
-                            // Install Terraform (assuming it's not pre-installed)
-                            sh 'curl -O https://releases.hashicorp.com/terraform/0.15.4/terraform_0.15.4_linux_amd64.zip'
-                            sh 'unzip terraform_0.15.4_linux_amd64.zip'
-                            sh 'mv terraform /usr/local/bin/'
-                        }
+                    def environment = params.server
+                    if (environment == 'dev') {
+                        tfVars_filename = "dev.tfvars"
+                        sh 'terraform workspace new dev || true' 
+                        sh 'terraform workspace select dev '
+                    } else if (environment == 'prod') {
+                        tfVars_filename = "prod.tfvars"
+                        sh 'terraform workspace new prod || true' 
+                        sh 'terraform workspace select prod '
                     }
                 }
             }
         }
-        
-        stage('Apply Terraform') {
+
+        stage('Terraform Plan') {
             steps {
                 script {
-                    def environment = params.ENVIRONMENT
-                    
-                    // Change to the Terraform code directory
-                    dir('repo') {
-                        // Perform any pre-Terraform steps if needed
-                        
-                        // Run Terraform apply command for the selected environment
-                        sh "terraform init"
-                        sh "terraform apply -var-file=${environment}.tfvars"
-                        
-                        // Perform any post-Terraform steps if needed
+                     if (environment == 'dev') {
+                        tfVars_filename = "dev.tfvars"
+                        sh 'terraform workspace select dev '
+                    } else if (environment == 'prod') {
+                        tfVars_filename = "prod.tfvars"
+                        sh 'terraform workspace select prod '
+                    }
+                     // Run Terraform plan command for the selected environment
+                    sh "terraform plan -var-file=${tfVars_filename}"
+                }
+            }
+        }
+
+       stage('Terraform Apply') {
+            steps {
+                // Run Terraform apply with approval prompt
+                script {
+                    def approval = input(
+                        id: 'terraform-apply-approval',
+                        message: 'Do you want to perform these actions? Enter "yes" to approve.',
+                        parameters: [
+                            [$class: 'TextParameterDefinition', defaultValue: '', description: '', name: 'approval']
+                        ]
+                    )
+
+                    if (approval.trim() == 'yes') {
+                        withCredentials([string(credentialsId: 'tfVars_filename', variable: 'tfVars_filename')]) {
+                            sh "terraform apply -var-file=${tfVars_filename}"
+                        }
+                    } else {
+                        error('Approval not granted. Aborting Terraform apply.')
                     }
                 }
             }
